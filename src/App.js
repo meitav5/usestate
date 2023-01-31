54e9b05de664a6a41ece64fbd1eee6377f07e36f
Meitav On <meitav102@gmail.com>
2023-01-31 10:52:32 +0200

    exercise3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Counter from './Counter';
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const {maxCounter, setMaxCounter} = useState(10);
+  const [delta, setDelta] = useState(1);
+  function handleDelta(e){
+    setDelta(Number(e.target.value))
+}
+function handleMax(e){
+  setMaxCounter(Number(e.target.value))
+}
   return (
     <div className="App">
       usestate
-      <Counter />
+      <input type = "number" value = {delta} onChange = {handleDelta}/>
+      <input type = "number" value = {maxCounter} onChange = {handleMax}/>
+      <Counter delta = {delta} maxCounter = {maxCounter}/>
+      <Counter delta = {delta} maxCounter = {maxCounter}/>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,27 @@
 import { useState } from "react";
-function Counter() {
+function Counter(props) {
+    const {delta, maxCounter} = props
     const [count, setCount] = useState(1);
     function incrs(){
         setCount(
             function(oldCount){
-                return oldCount + 1
+              if (oldCount + delta > maxCounter) return 0
+              return oldCount + delta
             }
         )
     }
     function reset(){
-        setCount(0)
+        setCount(
+          function(oldCount){
+            return oldCount + delta
+          }
+        )
     }
     return (
       <div className="App">
         <h1>Counter</h1>
         <p>Counter is at {count}</p>
-        <button onClick={incrs}>Click to add 1</button>
+        <button onClick={incrs}>Click to add {delta}</button>
         <button onClick={reset}>Click to reset</button>
       </div>
     );
